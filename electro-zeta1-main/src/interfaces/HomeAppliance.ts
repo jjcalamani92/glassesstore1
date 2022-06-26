aafee1fb816185e5e20e5c1d86622d9606b973e6
Jesus Calamani <jesuscalamani92@gmail.com>
2022-06-26 14:35:11 -0400

    v

---
+++ electro-zeta1-main/src/interfaces/HomeAppliance.ts
@@ -0,0 +1,29 @@
+export interface IHomeAppliance {
+	_id: string;
+	name: string;
+	brand: string;
+	image: string[];
+	description: string;
+	inStock: number;
+	slug: string;
+	category: string;
+	section: string;
+	item: string;
+	price: number;
+	oldPrice: number;
+	tags: string[];
+	client: string;
+	status: boolean;
+	site: string;
+	
+	color: string;
+	sizes: string[];
+}
+
+export interface IMark {
+	_id: string;
+	name: string;
+	href: string;
+	image: string;
+	description: string;
+	}
